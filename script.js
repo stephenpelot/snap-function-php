fb3c6dc88dcbe4aa3755a2968be213add89a985a
Stephen Pelot <spelot@cnm.edu>
2018-10-17 08:32:00 -0600

    commit 1

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTwoNumbers (a, b){
+	sum = a + b;
+	return sum;
+}
+
+function stringShenanigans (c, d) {
+answer = "c" + "d";
+	return answer;
+}
+
+function arrayActivate (){
+	return array1;
+}
+
+var array1
+{
+	[1, 2, 3];
+}
